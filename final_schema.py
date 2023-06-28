8ec9f0df92c92818bd62b79147fd6bde6b792ffe
medcoughlin <62365593+medcoughlin@users.noreply.github.com>
2023-06-27 23:28:08 -0400

    s

---
+++ final_schema.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import zipfile
+import matplotlib.pyplot as plt
+import math
+import re
+import datetime
+py -m !pip install --upgrade pip setuptools wheel
+from shapely import wkt
+import urllib
+import json
+import csv
+import requests
---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import zipfile
+import matplotlib.pyplot as plt
+import math
+import re
+import datetime
+from shapely import wkt
+import urllib
+import json
+import csv
+import requests
+import jdc
